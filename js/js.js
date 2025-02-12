3584d1dad0c6c3915641fc9356640a1bd29c1fcb
MarBProduction <martinboesen@hotmail.com>
2025-02-12 12:49:59 +0100

    Add feature to switch slide using arrow icons

--- js/js.js
+++ js/js.js
@@ -1,21 +1,72 @@
-const pages = document.querySelectorAll(".pages")
-const menuButtonMobile = document.querySelectorAll("#menu-mobile div")
+document.addEventListener("DOMContentLoaded", function () {
 
-pages.forEach(page => {
-  page.style.display = "none"
-})
+  const pages = document.querySelectorAll(".pages")
 
-function changePage(e, pageId){
+  /* ---------- CHANGE SLIDE ---------- */
   pages.forEach(page => {
-    if (page.id === pageId) {
-      page.style.display = "flex"
-      return
+    const slider = page.querySelector(".slider-container")
+    const rightArrow = page.querySelector(".arrow-right")
+    const leftArrow = page.querySelector(".arrow-left")
+    const scrollAmount = slider.clientWidth
+
+    if (rightArrow && leftArrow) {
+      rightArrow.addEventListener("click", () => {
+        slider.scrollBy({left: scrollAmount, behavior: "smooth"})
+      })
+
+      leftArrow.addEventListener("click", () => {
+        slider.scrollBy({left: -scrollAmount, behavior: "smooth"})
+      })
     }
+  })
+
+
+  /* ---------- SWITCH PAGE ---------- */
+  const menuButtonMobile = document.querySelectorAll("#menu-mobile div")
+
+  function changePage(e, pageId){
+    pages.forEach(page => {
+      if (page.id === pageId) {
+        page.style.display = "flex"
+        return
+      }
+      page.style.display = "none"
+    })
+
+    menuButtonMobile.forEach(menuButton => {
+      menuButton.classList.remove("main-active")
+    })
+    e.target.classList.add("main-active")
+  }
+
+  pages.forEach(page => {
     page.style.display = "none"
   })
-
   menuButtonMobile.forEach(menuButton => {
-    menuButton.classList.remove("main-active")
+    switch (menuButton.id) {
+      case "project-button":
+        menuButton.addEventListener("click", (event) => {
+          changePage(event, "projects")
+        })
+        break;
+      
+      case "about-me-button":
+        menuButton.addEventListener("click", (event) => {
+          changePage(event, "about-me")
+        })
+        break;
+      
+      case "contact-button":
+        menuButton.addEventListener("click", (event) => {
+          changePage(event, "contact")
+        })
+        break;
+    
+      default:
+        break;
+    }
   })
-  e.target.classList.add("main-active")
-}
+  pages[1].style.display = "flex" //TODO delete later, when new top is made
+  menuButtonMobile[1].classList.add("main-active") //TODO delete later, when new top is made
+
+});
